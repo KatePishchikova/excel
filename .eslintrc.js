533ad78ad844ca2c1d88f846f234de1a897d4302
Kate Pishchikova <kate.pishchikova@gmail.com>
2021-01-23 22:34:36 +0200

    Finish project config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'rules': {
+    'semi': 'off',
+    'linebreak-style': 'off',
+    'comma-dangle': 'off',
+    'require-jsdoc': 'off'
+  },
+};
+
+
