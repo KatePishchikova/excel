19f772f91c157a1f7fca3d63a85f9ef1b4e1fa41
KatePi <59488900+KatePishchikova@users.noreply.github.com>
2021-01-29 02:25:37 +0200

    Framework start (#3)
    * Finish base framework functional
    
    * comment debug

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,11 +11,14 @@ module.exports = {
     'ecmaVersion': 12,
     'sourceType': 'module',
   },
+  'parser': 'babel-eslint',
   'rules': {
     'semi': 'off',
     'linebreak-style': 'off',
     'comma-dangle': 'off',
-    'require-jsdoc': 'off'
+    'require-jsdoc': 'off',
+    'object-curly-spacing': 'off',
+    'arrow-parens': 'off'
   },
 };
 
