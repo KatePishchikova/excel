58d423cbb1a82607af8b6411d8844d3848ebbec1
Kate Pishchikova <kate.pishchikova@gmail.com>
2021-02-07 01:40:29 +0200

    Finish base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,18 +4,41 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
+    this.emitter = options.emitter
+    this.unsubckribers = []
+
+    // Настраиваем наш компонент до init
+    this.prepare()
   }
+
+  prepare() {}
 
   // Возвращает шаблон компонента
   toHTML() {
-    return '';
+    return ''
   }
 
+  // Инициализируем компонент
+  // Добавляем ДОМ сушателей
   init() {
     this.initDOMListeners()
   }
 
+  // Уведомляем слушателя о событии event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  // Подписываемся на событие event
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubckribers.push(unsub)
+  }
+
+  // Удаляем компонент
+  // Чистим ДОМ сушателей
   destroy() {
     this.removeDOMListeners()
+    this.unsubckribers.forEach(unsub => unsub())
   }
 }
